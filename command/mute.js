a326524fecd17bcbafd8ac83bd56ee907a12d1db
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-20 17:57:22 +0700

    Update mute

--- command/mute.js
+++ command/mute.js
@@ -4,30 +4,30 @@ module.exports = (client) => {
   command(client, 'mute', (message) => {
     const { member, mentions } = message
 
-    const tag = `<@${member.id}>`
     if (member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)){
       const target = mentions.users.first()
       if (target) {
         const targetMember = message.guild.members.cache.get(target.id)
-        console.log('Mute', message.author.username);
-    let mutedRole = message.guild.roles.cache.find(
-      role => role.name === 'Muted'
-    );
-    let defaultRole = message.guild.roles.cache.find(
-      role => role.name === 'Member'
-    );
-
-    targetMember.roles.add(mutedRole);
-    targetMember.roles.remove(defaultRole);
-
-
-        message.reply(`That user has been muted,`)
+        console.log(message.author.username,' use mute command');
+        let mutedRole = message.guild.roles.cache.find(
+            role => role.name === 'Muted'
+          );
+          let role = message.guild.roles.cache.find(
+            role => role.name === 'Member'
+          );
+      
+          targetMember.roles.add(mutedRole);
+          targetMember.roles.remove(role);
+      
+        const tag = `<@${targetMember.id}>`
+        message.reply(`${tag} has been muted,`)
       } else {
         message.reply(`Please tag someone to muted.`)
       }
     } else {
-      message.reply(
-        `You do not have any permission to use this command.`
+        message.delete();
+        message.author.send(
+        `You do not have any permission to use mute command in kumi sever.`
       )
     }
   })
