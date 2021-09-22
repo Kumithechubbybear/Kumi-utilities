1fe606d5c43939d2916d7fe653b7176507ac9000
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-22 21:51:34 +0700

    Update website.js

--- command/website.js
+++ command/website.js
@@ -4,12 +4,20 @@ const prefix = process.env.PREFIX;
 
 const info = new MessageEmbed()
 	.setDescription('🖥️ see update form us at <#884435292541575248> also you can contribute on github and it will show on <#887327669451120681>')
+const infoo = new MessageEmbed()
+	.setDescription('🖥️ We send kumi website link in your DM')
 
 
 module.exports = (client) => {
     client.on('message', (message) => {
         if (message.content.startsWith( prefix + 'website')) {
             message.reply({ embeds: [info] });
+        }          
+    })
+    client.on('message', (message) => {
+        if (message.content.startsWith( prefix + 'kumiweb')) {
+            message.reply({ embeds: [infoo] });
+            message.author.send('https://kumithechubbybear.netlify.app/')
         }          
     })
 
