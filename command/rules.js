d318e181b3aef938e3ea8ab4d5ba96bd3c63d1fd
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-30 11:23:41 +0700

    Update rules

--- command/rules.js
+++ command/rules.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const prefix = process.env.PREFIX;
 
 const info = new MessageEmbed()
-	.setDescription('📰 You can read rules in <#884401760435834890>')
+	.setDescription('📰 You can read rules in <#884401760435834890> or use `>rules <rules number>`')
 
 const rules1 = new MessageEmbed()
-	.setDescription('')
+    .setTitle('Rules 1')
+	.setDescription('**Do not organize, participate in, or encourage harassment of others.** Disagreements happen and are normal, but continuous, repetitive, or severe negative comments may cross the line into harassment and are not okay.')
 
 
 module.exports = (client) => {
@@ -15,5 +16,9 @@ module.exports = (client) => {
             message.reply({ embeds: [info] });
         }          
     })
-
+    client.on('message', (message) => {
+        if (message.content === ( prefix + 'rules 1')) {
+            message.channel.send({ embeds: [rules1] });
+        }          
+    })
 }
