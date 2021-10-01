075c6e8467cabc4fb24a47b410f4538800d43ed9
ronnapatp <ronnapatp@users.noreply.github.com>
2021-10-01 18:00:16 +0700

    Update

--- command/pingrole.js
+++ command/pingrole.js
@@ -1,9 +1,6 @@
 const pre = '!'
 
 
-
-
-
 module.exports = (client) => {
     client.on('message', (message) => {
         if (message.content ===(pre +  'websiterole')) {
@@ -15,6 +12,23 @@ module.exports = (client) => {
           
                   message.member.roles.add(muteRole);
                 message.author.send('**Web update ping** role just added to you')
+            } else {
+                message.delete();
+                message.author.send('Use this command in <#893443548375035955>')
+            }       
+        }     
+    })
+
+    client.on('message', (message) => {
+        if (message.content ===(pre +  'promotionrole')) {
+            if (message.channel.id === '893443548375035955') {
+                message.delete();
+                let muteRole = message.guild.roles.cache.find(
+                  role => role.name === 'Promotion ping'
+              );
+          
+                  message.member.roles.add(muteRole);
+                message.author.send('**Promotion ping** role just added to you')
             } else {
                 message.delete();
                 message.author.send('Use this command in <#893443548375035955>')
