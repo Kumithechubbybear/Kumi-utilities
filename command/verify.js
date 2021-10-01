ec55c655a60117620009464565264319c52c0eca
ronnapatp <ronnapatp@users.noreply.github.com>
2021-10-01 20:23:20 +0700

    Fix somethings

--- command/verify.js
+++ command/verify.js
@@ -15,9 +15,7 @@ module.exports = (client) => {
             } else {
                 message.delete();
             }       
-        } else {
-            message.delete();
-        }     
+        }  
     })
 
 }
