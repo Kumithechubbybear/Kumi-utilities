55360d804becfde0d2b256e73e440bf9078b421f
ronnapatp <ronnapat.sri@gmail.com>
2021-10-12 10:15:33 +0700

    Update help command

--- command/help.js
+++ command/help.js
@@ -10,6 +10,9 @@ const info = new MessageEmbed()
 		{ name: '`>faq`', value: 'Show **FAQ**', inline: true },
 		{ name: '`>website`', value: 'Show website update channel', inline: true },
         { name: '`>joke`', value: 'Random joke', inline: true },
+        { name: '`>info`', value: 'Show info about server', inline: true },
+        { name: '`>website`', value: 'Show info about website', inline: true },
+        { name: '`>kumiweb`', value: 'Show kumi website', inline: true },
 	)
 
 module.exports = (client) => {
