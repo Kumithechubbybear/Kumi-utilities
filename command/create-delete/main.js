8a68356399c9552e287bdc7ee81b4431d63ede36
Ronnapat Srivoravilai <ronnapat.sri@gmail.com>
2021-12-06 11:43:49 +0700

    Update create channel

--- command/create-delete/main.js
+++ command/create-delete/main.js
@@ -4,14 +4,8 @@ const command = require('../command')
 const { Permissions } = require('discord.js');
 
 module.exports = (client) => {
-    command(client, 'create', (message) => {
-      const { member, mentions } = message
-  
-      if (
-          member.permissions.has(Permissions.FLAGS.ADMINISTER)
-      ) {
-        const target = mentions.users.first()
-        if (target) {
+  client.on('message',(message) => {
+    if (message.content ===( prefix + 'create')) {
           message.guild.channels.create(`channel`, { 
             type: 'text', 
             permissionOverwrites: [{ 
@@ -19,15 +13,6 @@ module.exports = (client) => {
                 allow: ['VIEW_CHANNEL'],
             }]
         });
-        message.channel.send("Channel Created!")
-        } else {
-          message.reply(`IDK`)
-        }
-      } else {
-          message.delete();
-        message.author.send(
-          `You do not have any permission to create channel`
-        )
       }
-    })
-  }
+  })
+}
